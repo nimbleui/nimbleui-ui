2adca6ea5139f6f0b0e8016709e9dff7bf5da675
陈羽云 <897908015@qq.com>
2023-04-27 12:24:56 +0800

    update：配置css、scss样式格式化

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,9 +1,6 @@
 module.exports = {
-  extends: ["stylelint-config-standard-scss", "stylelint-config-standard", "stylelint-config-recess-order"],
-  plugins: ["stylelint-scss", "stylelint-order"],
+  extends: ["stylelint-config-standard", "stylelint-config-standard-scss"],
   rules: {
-    "no-missing-end-of-source-newline": null,
-    "declaration-block-no-duplicate-properties": true,
-    "no-invalid-double-slash-comments": null,
+    indentation: 2,
   },
 };
