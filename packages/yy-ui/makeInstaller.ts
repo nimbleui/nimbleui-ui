9f8680013b3ba16d401da1400ce64be52be1ba84
陈羽云 <897908015@qq.com>
2023-07-21 15:23:59 +0800

    feat：添加注册是支持调整主题颜色

--- packages/yy-ui/makeInstaller.ts
+++ packages/yy-ui/makeInstaller.ts
@@ -6,14 +6,16 @@ import type { Opts } from "@yy/hooks";
 
 export interface OptionsProps {
   theme?: Opts;
+  // 是否暗黑主题
+  isDark?: boolean;
 }
 
 export const makeInstaller = (components: Plugin[] = []) => {
   const install = (app: App, options: OptionsProps = {}) => {
     components.forEach((c) => app.use(c));
-    const { theme, ...other } = options;
+    const { theme, isDark, ...other } = options;
     // 设置主题颜色
-    useTheme(theme, "dark");
+    useTheme(theme, isDark ? "dark" : "light");
     // 公共参数
     app.provide(configContextKey, other);
   };
