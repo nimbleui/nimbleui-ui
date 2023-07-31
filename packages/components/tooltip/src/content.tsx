b8e8db063767910b9586cab102706d4a5df85ea1
陈羽云 <897908015@qq.com>
2023-07-31 17:57:41 +0800

    fixed：优化tooltip组件

--- packages/components/tooltip/src/content.tsx
+++ packages/components/tooltip/src/content.tsx
@@ -22,7 +22,7 @@ export default defineComponent({
 
     const { lazyRender } = useLazyRender(() => props.show);
     useScrollParent(tooltipContext.triggerRef, () => {
-      if (tooltipContext.contentRef.value) {
+      if (tooltipContext.contentRef.value && props.show) {
         handleEnter(tooltipContext.contentRef.value);
       }
     });
