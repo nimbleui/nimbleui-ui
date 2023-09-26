5f51eca1c84003554f54a74cbb97cb9682943490
陈羽云 <897908015@qq.com>
2023-09-26 17:25:51 +0800

    fixed：修复scrollbar有内边距滚动条位置错误问题

--- packages/components/scrollbar/src/scrollbar.tsx
+++ packages/components/scrollbar/src/scrollbar.tsx
@@ -31,9 +31,11 @@ export default defineComponent({
 
     const getElementRect = () => {
       const type = props.xScroll ? "offsetWidth" : "offsetHeight";
+      const scrollType = props.xScroll ? "scrollWidth" : "scrollHeight";
+
       const bar = barRef.value?.[type] || 0;
       const wrap = wrapRef.value?.[type] || 0;
-      const resize = resizeRef.value?.[type] || 0;
+      const resize = wrapRef.value?.[scrollType] || 0;
       Object.assign(clientRect, {
         bar,
         wrap,
@@ -55,9 +57,10 @@ export default defineComponent({
         const { disY, disX } = data;
         const { wrap, resize, bar } = clientRect;
         const diff = resize - wrap;
+
         const barSize = barSizeRef.value;
         const dis = xScroll ? disX : disY;
-        const dScrollTop = (dis * diff) / (bar - barSize);
+        const dScrollTop = (Math.round(dis) * diff) / Math.round(bar - barSize);
 
         const scrollTop = dScrollTop + memoYTop;
         if (wrapRef.value) {
@@ -82,8 +85,8 @@ export default defineComponent({
 
     // 滚动条的距离
     const barDisRef = computed(() => {
-      const { bar, wrap, resize, scroll } = clientRect;
-      const diff = resize - wrap;
+      const { bar, resize, scroll } = clientRect;
+      const diff = resize - bar;
 
       return diff ? (scroll / diff) * (bar - barSizeRef.value) : 0;
     });
@@ -117,19 +120,20 @@ export default defineComponent({
     });
     const handleHover = (isEnter: boolean) => {
       return () => {
-        getElementRect();
+        isEnter && getElementRect();
         hoverRef.value = isMove.value ? true : isEnter;
       };
     };
 
     return () => {
-      const { tag: Component, contentClass, contentStyle, native, xScroll } = props;
+      const { tag: Component, contentClass, contentStyle, native, xScroll, wrapClass, wrapStyle } = props;
       return (
         <div onMouseenter={handleHover(true)} onMouseleave={handleHover(false)} class={bem.b()} ref={scrollbarRef}>
           <div
             ref={wrapRef}
             onScroll={onScroll}
-            class={[bem.e("wrap"), !native ? bem.m("hidden-bar", "wrap") : undefined]}
+            style={wrapStyle}
+            class={[bem.e("wrap"), !native ? bem.m("hidden-bar", "wrap") : undefined, wrapClass]}
           >
             <Component
               style={[contentStyle, { width: xScroll ? "fit-content" : undefined }]}
